aa5bc2ba4ea60b645c249dc9aeecfccaf1f3be96
Skrynnikov_Vladislav <vldskrnnkv@gmail.com>
2020-11-09 23:41:54 +0300

    Upgrade of hello.py

--- hello.py
+++ hello.py
@@ -1,62 +1,37 @@
-from math import log
-import matplotlib.pyplot as graph_task_a
-import matplotlib.pyplot as graph_task_b
+from math import log10
+import matplotlib.pyplot as plt
 import numpy as np
 
 def calc(a, x):
-    t = pow(x, 2) - 1
-    y = pow(a, t) - log(t, 10) + pow(t, (1 / 3))
+    t = x ** 2 - 1
+    y = a ** t - log10(t) + t ** (1 / 3)
     return y
 
-def task_a(a, xn, xk, dx):
-    x = xn
-    x_arr = list()
-    y = list()
-    while x <= xk:
-        res = calc(a, x)
-        y.append(res)
-        x_arr.append(x)
-        x += dx
-    return y, x_arr
-
-def task_b(a, x_arr):
-    y = list()
-    for x in x_arr:
-        res = calc(a, x)
-        y.append(res)
-    return y
+def func(a, X):
+    Y = list()
+    for elem in X:
+        res = calc(a, elem)
+        Y.append(res)
+    return Y
 
 if __name__ == '__main__':
     a = 2.25
-    xn = 1.2
-    xk = 2.7
-    dx = 0.3
+    x_beg = 1.2
+    x_end = 2.7
+    x_del = 0.3
+    fig, axs = plt.subplots(1, 2, figsize = (8, 4))
 
-    a_res, x_arr_task_a = task_a(a, xn, xk, dx)
+    X = np.arange(x_beg, x_end, x_del)
+    Y = func(a, X)
+    axs[0].plot(X, Y)
+    axs[0].grid()
+    axs[0].set(xlabel = 'Параметр A', ylabel = 'Функция A', title = 'График A')
 
-    x_arr_task_b = [1.31, 1.39, 1.44, 1.56, 1.92]
+    X = [1.31, 1.39, 1.44, 1.56, 1.92]
+    Y = func(a, X)
+    axs[1].plot(X, Y)
+    axs[1].grid()
+    axs[1].set(xlabel = 'Параметр B', ylabel = 'Функция B', title = 'График B')
 
-    b_res = task_b(a, x_arr_task_b)
-
-    print(a_res)
-    print(x_arr_task_a)
-    print(b_res)
-    print(x_arr_task_b)
-
-    fig1, ax1 = graph_task_a.subplots()
-    fig2, ax2 = graph_task_b.subplots()
-
-    ax1.set(xlabel = 'Parameter_a', ylabel = 'Value_a', title = 'task_a')
-    ax2.set(xlabel = 'Parameter_b', ylabel = 'Value_b', title = 'task_b')
-    
-    ax1.grid()
-    ax2.grid()
-
-    ax1.plot(x_arr_task_a, a_res)
-    ax2.plot(x_arr_task_b, b_res)
-
-    fig1.savefig('task_a.png')
-    fig2.savefig('task_b.png')
-
-    graph_task_a.show()
-    graph_task_b.show()
+    fig.savefig("Graph.png")
+    plt.show()

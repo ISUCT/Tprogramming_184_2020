198baf6c87bba121c4374c6b269dba8b1ee43f6a
Polina Golubeva <golubeva.polina2001@yandex.ru>
2020-11-21 20:43:41 +0300

    Add sample work with numpy

--- function.py
+++ function.py
@@ -1,53 +1,74 @@
+import numpy as np
 import matplotlib.pyplot as plt
-import math
+# import math
 
 def calc(a, b, x):
-    if (x < 5):
-        numerator = math.pow(math.log10(math.pow(a, 2) + x), 2)
-        denominator = math.pow(a + x, 2)
+     if (x < 5):
+        numerator = np.pow(np.log10(np.pow(a, 2) + x), 2)
+        denominator = np.pow(a + x, 2)
         y = numerator / denominator
         return y
-    else:
-        numerator = math.pow(a + (b * x), 2.5)
-        denominator = 1.8 + math.pow(math.cos(a * x), 3)
+     else: 
+        numerator = np.pow(a + (b * x), 2.5)
+        denominator = 1.8 + np.pow(np.cos(a * x), 3)
         y = numerator / denominator
         return y
+
+def calc_np(a, b, x):
+     if (x < 5):
+        numerator = np.pow(np.log(np.pow(a, 2) + x), 2)/np.log(10)
+        denominator = np.pow(a + x, 2)
+        y = numerator / denominator
+        return y
+     else:
+        numerator = np.pow(a + (b * x), 2.5)
+        denominator = 1.8 + np.pow(np.cos(a * x), 3)
+        y = numerator / denominator
+        return y
+
+def task_a_np(a, b, xn, xk, dx):
+    x = np.arange(xn, xk, dx)
+    y = calc_np(a, b, x)
+    return x, y
 
 def task_a(a, b, xn, xk, dx):
-    x = xn
-    result = []
-    while x <= xk:
-        y = calc(a, b, x)
-        result.append([x, y])
-        x += dx
-    return result
+    i = xn
+    
+    res = []
+    while i <= xk:
+        y = calc(a, b, i)
+        res.append((i, y))
+        i += dx
+    return res
 
 def task_b(a, b, x_arr):
-    result = []
+    y = []
     for x in x_arr:
-        y = calc(a, b, x)
-        result.append([x, y])
-    return result
+        res = calc(a, b, x)
+        y.append(res)
+    return y
 
+def draw(x, y):
+    fig = plt.plot(x, y, 'rx')
+    fig[0].figure.add_axes(xlabel="asdfasdf")
+    plt.grid()
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("My graphics")
+    plt.show()
 
 if __name__ == "__main__":
     a = -2.5
     b = 3.4
-    xn = 3.5
-    xk = 6.5
-    dx = 0.6
+    x = 3.5
+    # y = calc(a, b, x)
+    # print(f"x={x:.3f} y={y:.3f}")
 
-    y = calc(a, b, xn)
-    print(f'x={xn:.3f} y={y:.3f}')
 
-    a_res = task_a(a, b, xn, xk, dx)
-    print(a_res)
+    x, y = task_a_np(a, b, 3.5, 6.5, 0.6)
+    draw(x, y)
+    # print(a_res)
 
-    x_arr = [2.89, 3.54, 5.21, 6.28, 3.48]
-    b_res = task_b(a, b, x_arr)
-    print(b_res)
-
-    plt.plot(*zip(*a_res), 'b')
-    plt.plot(*zip(*b_res), 'r')
-    plt.show()
-    
+    # x_arr = [2.89, 3.54, 5.21, 6.28, 3.48]
+    # b_res = task_b(a, b, x_arr)
+    # print(b_res)

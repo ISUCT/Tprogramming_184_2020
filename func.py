da92c6af6ebd46108b4379faa9c033c96398132f
Stanislav Fokin <stasfokin86@gmail.com>
2021-01-15 02:19:19 +0300

    last version

--- func.py
+++ func.py
@@ -1,23 +1,48 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
-def calc(a, b, x):
-    num = pow(a,1/3)+pow(math.tan(b*x),4.5)
-    denum = pow(b,1/5)+pow(1/math.tan(a*x),2.7)
+def calc_np(a, b, x):
+    num = a**1/3+pow(np.tan(b*x),4.5)
+    denum = b**1/5+pow(1/np.tan(a*x),2.7)
     y = num/denum
     return y
 
+def task_b(a, b, x_arr):
+    y = []
+    for x in x_arr:
+        res = calc_np(a, b, x)
+        y.append(res)
+    return y
+
+def draw(xs, ys):
+    fig = plt.subplots()
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("My graphics")
+    plt.plot(xs,ys)
+    plt.show()
+
+
 a = 0.1
 b = 0.5
-x = 0.33
-x1 = 1.23
+xn = 0.33
+xk = 1.23
 dx = 0.18
+x_arr = [0.5, 0.36, 0.40, 0.62, 0.78]
+y = [task_b(a,b, x_arr)]
+
 xs = []
 ys = []
 
-while x<=x1:
-    y = calc(a,b,x)
-    xs.append(x)
+while xn<=xk:
+    y = calc_np(a, b, xn)
+    xs.append(xn)
     ys.append(y)
-    x+=dx
+    xn+=dx
 
-print (f"x={xs} y={ys}")
+print()
+print(f"Task A: x={xs} y={ys}")
+print()
+print(f"Task B: x1={x_arr} y1={y}")
+draw(xs, ys)
